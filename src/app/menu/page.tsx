b9e4486a5f7ad4bfd250dd440d60e15f46301633
MolchanovAlexander <rybkolhoz@gmail.com>
2023-09-12 12:44:05 +0300

    fullUI

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 import React from "react";
 
 const MenuPage = () => {
-  let style = "bg-black-500"
-  let style2 = "bg-white-500"
+ 
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center ">
       {menu.map((category) => (
