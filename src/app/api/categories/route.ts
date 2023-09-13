c8ba1f356eee4c85fb299e8a8b6c3a57664e0628
MolchanovAlexander <rybkolhoz@gmail.com>
2023-09-13 11:37:49 +0300

    26

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,8 +1,28 @@
-import { NextResponse } from "next/server"
 
-export const GET =()=> {
-    return new NextResponse(":noob", {status:200}) 
-}
-export const POST =()=> {
-    return new NextResponse(":noob", {status:200}) 
-}
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+// FETCH ALL CATEGORIES
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(JSON.stringify(categories), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
+
+
+// // fetch
+// export const GET =()=> {
+//     return new NextResponse(":noob", {status:200}) 
+// }
+// export const POST =()=> {
+//     return new NextResponse(":noob", {status:200}) 
+// }
