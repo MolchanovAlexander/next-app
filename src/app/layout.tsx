430e941d5e82e19967d6254513b0f0fbcfcf6127
MolchanovAlexander <rybkolhoz@gmail.com>
2023-10-07 20:07:11 +0300

    ere

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from '@/components/Navbar'
 import Notification from '@/components/Notification'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+//import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
 import AuthProvider from '@/components/AuthProvider'
 import QueryProvider from '@/components/QueryProvider'
@@ -10,7 +10,7 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 
-const inter = Inter({ subsets: ['latin'] })
+//const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Newbie restaurant',
@@ -24,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+     <body className=""> 
         <AuthProvider>
           <QueryProvider>
             <Notification />
