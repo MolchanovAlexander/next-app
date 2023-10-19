53879de9d042e8ca2bc51e658b3c989ad294a42a
MolchanovAlexander <rybkolhoz@gmail.com>
2023-10-19 22:27:55 +0300

    swiperAdd

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import Slideshow from "./Slideshow";
 const data = [
     {
         id: 1,
@@ -33,7 +34,7 @@ const Slider = () => {
     return (
         <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
             {/* text container */}
-            <div className="flex-1 flex items-center justify-center 
+            <div className="flex-1 flex h-full items-center justify-center 
             flex-col gap-8 text-red-500 font-bold ">
                 <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
                     {data[currentSlide].title}
@@ -41,8 +42,9 @@ const Slider = () => {
                 <Link href={`/menu`} className="bg-red-500 text-white py-4 px-8">Order now</Link>
             </div >
             {/* img container */}
-            <div className="w-full flex-1 relative ">
-                <Image src={data[currentSlide].image} alt="" fill className="object-cover" />
+            <div className="flex flex-1 relative w-full h-full lg:w-1/2   ">
+                {/* <Image src={data[currentSlide].image} alt="" fill className="object-cover" /> */}
+                <Slideshow />
             </div>
         </div>
     );
