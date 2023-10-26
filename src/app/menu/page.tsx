e28eb7913ed1a0a6f6bad1b139395d594ad2df97
MolchanovAlexander <rybkolhoz@gmail.com>
2023-10-26 15:43:15 +0300

    cacheminus

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -18,34 +18,34 @@ const getData = async () => {
 
 const MenuPage = async () => {
   
-  // const menu: MenuType = await getData();
+  const menu: MenuType = await getData();
   
   
-  // return (
-  //   <div className="flex flex-col h-[calc(100vh-6rem)]">
-  //     <AddProductButton/>
-  //     <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex 
-  //   flex-col md:flex-row items-center ">
+  return (
+    <div className="flex flex-col h-[calc(100vh-6rem)]">
+      <AddProductButton/>
+      <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex 
+    flex-col md:flex-row items-center ">
 
-  //       {menu.map((category) => (
-  //         <Link
-  //           href={`/menu/${category.slug}`}
-  //           key={category.id}
-  //           className="w-full h-1/3 bg-cover p-8 md:h-1/2"
-  //           style={{ backgroundImage: `url(${category.img})` }}
-  //         >
-  //           <div className={`text-${category.color} w-2/3 h-full flex flex-col justify-around`}>
-  //             <h1 className="uppercase  font-bold text-3xl">{category.title}</h1>
-  //             <p className="text-sm my-8 md:my-1 h-[60%] overflow-hidden">{category.desc}</p>
-  //             <button
-  //               className={`hidden 2xl:block bg-${category.color} text-2xl 
-  //             text-${category.color === "black" ? "white" : "red-500"}
-  //              py-2 px-4 rounded-md`}>Explore</button>
-  //           </div>
-  //         </Link>
-  //       ))}
-  //     </div>
-  //   </div>);
+        {menu.map((category) => (
+          <Link
+            href={`/menu/${category.slug}`}
+            key={category.id}
+            className="w-full h-1/3 bg-cover p-8 md:h-1/2"
+            style={{ backgroundImage: `url(${category.img})` }}
+          >
+            <div className={`text-${category.color} w-2/3 h-full flex flex-col justify-around`}>
+              <h1 className="uppercase  font-bold text-3xl">{category.title}</h1>
+              <p className="text-sm my-8 md:my-1 h-[60%] overflow-hidden">{category.desc}</p>
+              <button
+                className={`hidden 2xl:block bg-${category.color} text-2xl 
+              text-${category.color === "black" ? "white" : "red-500"}
+               py-2 px-4 rounded-md`}>Explore</button>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>);
 };
 
 export default MenuPage;
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -7,9 +7,7 @@ import React from "react";
 
 const getData = async (id: string) => {
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products/${id}`, {
-    cache: "no-store"
-  })
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products/${id}`)
   if (!res.ok) {
     throw new Error("Failed")
   }
