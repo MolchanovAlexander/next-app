4dcb3b99bd27f3d96d214d8ad35eeec4e65e2339
MolchanovAlexander <rybkolhoz@gmail.com>
2024-03-12 09:50:43 +0200

    5454

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -2,7 +2,7 @@ export default function Notification() {
     return (
       <div className='h-12 bg-red-600 text-white
        px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>
-        Notification delivery guf rip dniwe peregnoy order now befor 454545 too late!!!
+        Notification delivery guf rip dniwe peregnoy order now befor it&aposs too late!!!
       </div>
      )
   }
