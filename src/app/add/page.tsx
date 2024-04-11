0c0a6aae2e51fcd1da7b08874e2befe88bc225ac
MolchanovAlexander <rybkolhoz@gmail.com>
2024-04-11 19:03:22 +0300

    dff

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -85,6 +85,7 @@ const AddPage = () => {
 
     try {
       const url = await upload();
+      console.log(process.env.NEXT_PUBLIC_BASE_URL);
       
       const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`, {
         method: "POST",
